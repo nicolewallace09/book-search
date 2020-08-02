d7c9de1f5c141d61d9dce0acda22c1bb6f7b28e7
nicolewallace09 <nicole.elisaw@gmail.com>
2020-08-02 16:08:38 -0700

    adding apollo server to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,28 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+// react component to provide data to all of the other components
+import { ApolloProvider } from '@apollo/react-hooks';
+// get the data when we are ready to use it 
+import ApolloClient from 'apollo-boost';
+
+// takes in token from localStorage before each requests
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +35,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
