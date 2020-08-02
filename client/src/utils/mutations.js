5ddb84fb62a2258ba805126d882c83f944625cb5
nicolewallace09 <nicole.elisaw@gmail.com>
2020-08-02 16:02:34 -0700

    mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,21 +1,77 @@
-import gql from 'graphql-tag'; 
+import gql from 'graphql-tag';
 
-// queries for logged in users
-export const GET_ME = gql`
-{
-    me {
-        _id
-        username
-        email
-        bookCount 
-        saveBooks {
-            bookId 
-            title
-            description
-            authors
-            image
-            link
+// mutation for logged in user
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
         }
     }
-}
-`; 
+`
+
+// mutation for user to sign up 
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+        token
+            user {
+                _id
+                username
+                email
+                bookCount 
+                savedBooks {
+                    bookId
+                    title
+                    description
+                    authors
+                    image
+                    link
+                }
+            }
+        }
+    }
+`;
+
+// mutation to save books
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: booksInput!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }
+`;
+
+// mutation to remove books
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }
+`;
